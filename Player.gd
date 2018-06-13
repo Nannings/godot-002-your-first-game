1eecd4159a7b6f86a3ca1fc524235bb38011c7d3
DESKTOP-NV4V396\tablet1 <mrnannings@gmail.com>
2018-06-13 23:34:52 +0200

    part 2;

--- Player.gd
+++ Player.gd
@@ -1,15 +1,58 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+signal hit
+
+export (int) var SPEED
+var velocity = Vector2()
+var screensize;
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	hide()
+	screensize = get_viewport_rect().size
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		$AnimatedSprite.play()
+		velocity = velocity.normalized() * SPEED
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x);
+	position.y = clamp(position.y, 0, screensize.y);
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+	
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	call_deferred("set_monitoring", false)
+
+func  start(pos):
+	position = pos;
+	monitoring = true
+	
+		
+	
+	
